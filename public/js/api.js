65761f991854b71f350707ee7761ec104e64d2a7
imsk96 <77296075+imsk96@users.noreply.github.com>
2025-06-28 12:18:32 +0000

    update

--- public/js/api.js
+++ public/js/api.js
@@ -2,8 +2,8 @@
 
 // ⬇️ !! IMPORTANT !! ⬇️
 // Yahan Supabase se copy ki hui URL aur Key daalein
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'; // Isko replace karein
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // Isko replace karein
+const SUPABASE_URL = 'https://knjtpsygbbtjunethhoj.supabase.co'; // Isko replace karein
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuanRwc3lnYmJ0anVuZXRoaG9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExMTA0MTMsImV4cCI6MjA2NjY4NjQxM30.ILwWlSbymruyz_UZJXbtqbm6i3dVZNKWV0iqUK-Z4fY'; // Isko replace karein
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
